f9dc95962b3c8716d450deb7d8a7e55dd6c7a546
Shimouchi-Shumpei <107620906+Shimouchi-Shumpei@users.noreply.github.com>
2026-06-29 01:18:23 +0900

    Feat: MemberCard.tsx shadow

--- src/components/MemberCard.tsx
+++ src/components/MemberCard.tsx
@@ -11,7 +11,7 @@ export type MemberCardProps = {
 
 export default function MemberCard({ id, name, description, works, className = '' }: MemberCardProps) {
   return (
-    <section className={`flex flex-1 flex-col gap-6 rounded-2xl border border-amber-500 bg-white p-6 text-black sm:rounded-[2rem] sm:p-8 lg:rounded-[3rem] lg:p-16 ${className}`}>
+    <section className={`flex flex-1 flex-col gap-6 rounded-2xl border-2 border-orange-200 bg-white p-6 text-black shadow-[6px_6px_0_0_#f59e0b] duration-200 sm:rounded-[2rem] sm:p-8 sm:shadow-[8px_8px_0_0_#f59e0b] lg:rounded-[3rem] lg:p-16 ${className}`}>
       <div className="flex flex-col gap-8 lg:flex-row lg:items-center lg:justify-between lg:gap-16">
         <div className="flex min-w-0 flex-col gap-4 sm:gap-6">
           <span className='ml-1 text-base font-bold tracking-widest text-amber-600 sm:text-xl'>{id}</span>
@@ -30,5 +30,6 @@ export default function MemberCard({ id, name, description, works, className = '
     </section>
   )
 }
+
 
 
